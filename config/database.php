a9a5cdac4864ed3106c2ae6c2899107289f9bc6a
estebanbacl <estebanbacl@gmail.com>
2016-07-07 09:57:30 -0500

    db

--- config/database.php
+++ config/database.php
@@ -1,5 +1,13 @@
 <?php
 
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -44,6 +52,8 @@ return [
     |
     */
 
+
+
     'connections' => [
 
         'sqlite' => [
@@ -54,11 +64,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+           // 'port' => env('DB_PORT', '3306'),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             //'unix_socket'   => '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock', // XAMPP
             //'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock', // MAMP
             'charset' => 'utf8',
